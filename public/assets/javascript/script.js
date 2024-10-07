cb6c86594b0d72b6a658c3c84ec7df341d03523f
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-10-07 18:53:53 -0300

    Feito o compile do script.ts

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,9 +1,49 @@
-/** //TODO Eu planejo fazer um display grid para o site 
- *  dividir o conteudo assim:
- *  header
- * generators (div1 com 50% e div2 com 50% )
- * observação
- * copyright
- */
-
-//TODO terminar o foot
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GeradoresCPF = void 0;
+class GeradoresCPF {
+    gerarCPFValido() {
+        let cpf;
+        do {
+            cpf = this.gerarNumerosAleatorios();
+            const digito1 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO1);
+            const digito2 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO2);
+            cpf[9] = digito1;
+            cpf[10] = digito2;
+        } while (!this.validarCPF(cpf));
+        return cpf.join('');
+    }
+    gerarCPFNaoValido() {
+        let cpf;
+        do {
+            cpf = this.gerarNumerosAleatorios();
+            const digito1 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO1);
+            const digito2 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO2);
+            cpf[9] = digito1;
+            cpf[10] = digito2;
+        } while (this.validarCPF(cpf));
+        return cpf.join('');
+    }
+    gerarNumerosAleatorios() {
+        let array = [];
+        for (let index = 0; index < 11; index++) {
+            let valor = Math.floor(Math.random() * 10);
+            array.push(valor);
+        }
+        return array;
+    }
+    calcularDigitoVerificador(numeros, pesoMaximo) {
+        const soma = numeros.slice(0, pesoMaximo - 1).reduce((acc, num, index) => acc + num * (pesoMaximo - index), 0);
+        const resto = soma % 11;
+        return resto < 2 ? 0 : 11 - resto;
+    }
+    validarCPF(cpf) {
+        const digito1 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO1);
+        const digito2 = this.calcularDigitoVerificador(cpf, GeradoresCPF.PESO_MAXIMO_DIGITO2);
+        return cpf[9] === digito1 && cpf[10] === digito2;
+    }
+}
+exports.GeradoresCPF = GeradoresCPF;
+GeradoresCPF.TAMANHO_CPF = 11;
+GeradoresCPF.PESO_MAXIMO_DIGITO1 = 10;
+GeradoresCPF.PESO_MAXIMO_DIGITO2 = 11;
